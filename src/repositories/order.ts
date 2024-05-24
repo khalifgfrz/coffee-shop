b4fed4f5534a9e706fd427545814d681bd7b616a
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-05-24 22:57:48 +0700

    revisi page, query update dan returning

--- src/repositories/order.ts
+++ src/repositories/order.ts
@@ -5,22 +5,21 @@ import { IdataOrder, IorderBody, IorderQuery } from "../models/order";
 
 export const getAllOrder = (que: IorderQuery): Promise<QueryResult<IdataOrder>> => {
   let query = `select ol.id, ol."no_order", ol."date", p.product_name, ol.status, p.price, p2.promo_name from order_list ol
-    join product p on ol.product_id = p.id
-    join promo p2 on ol.promo_id = p2.id`;
-  const values = [];
-  const { limit, offset } = que;
-  if (limit) {
-    query += " limit $1";
-    values.push(`${limit}`);
-    if (offset) {
-      query += " offset $2";
-      values.push(`${offset}`);
-    }
-  } else if (offset) {
-    query += " offset $1";
-    values.push(`${offset}`);
+  join product p on ol.product_id = p.id
+  join promo p2 on ol.promo_id = p2.id`;
+  const { page } = que;
+  switch (page) {
+    case "1":
+      query += " limit 10 offset 0";
+      break;
+    case "2":
+      query += " limit 10 offset 10";
+      break;
+    case "3":
+      query += " limit 10 offset 20";
+      break;
   }
-  return db.query(query, values);
+  return db.query(query);
 };
 
 export const getOneOrder = (no_order: string): Promise<QueryResult<IdataOrder>> => {
@@ -34,21 +33,22 @@ export const getOneOrder = (no_order: string): Promise<QueryResult<IdataOrder>> 
 
 export const createOrder = (body: IorderBody): Promise<QueryResult<IdataOrder>> => {
   const query = `insert into order_list (product_id, promo_id, user_id, status) values ($1,$2,$3,$4)
-      returning *`;
+  returning product_id, promo_id, user_id, status`;
   const { product_id, promo_id, user_id, status } = body;
   const values = [product_id, promo_id, user_id, status];
   return db.query(query, values);
 };
 
 export const deleteOrder = (no_order: string): Promise<QueryResult<IdataOrder>> => {
-  const query = `delete from order_list where no_order=$1`;
+  const query = `delete from order_list where no_order=$1
+  returning product_id, promo_id, user_id, status`;
   const values = [no_order];
   return db.query(query, values);
 };
 
 export const updateOrder = (no_order: String, body: IorderBody): Promise<QueryResult<IdataOrder>> => {
   const query = `update order_list set status = $1, updated_at = now() where no_order = $2
-      returning *`;
+  returning product_id, promo_id, user_id, status`;
   const { status } = body;
   const values = [status, no_order];
   return db.query(query, values);
