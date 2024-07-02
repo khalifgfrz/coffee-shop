6185df71aad1d831f88ad15b46cd3625f3715d92
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-07-02 11:56:08 +0700

    edit

--- server.ts
+++ server.ts
@@ -25,7 +25,7 @@ app.use(logger);
 // cors
 const configs: CorsOptions = {
   origin: ["http://localhost:8080", "http://127.0.0.1:5500", "http://localhost:5173", "https://coffee-shop-fe-react.vercel.app"],
-  methods: ["POST", "PATCH"],
+  methods: ["POST", "PATCH", "GET"],
   allowedHeaders: ["Authorization", "x-headers", "content-type"],
 };
 app.use(cors(configs));
